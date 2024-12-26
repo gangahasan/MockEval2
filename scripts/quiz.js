f38632cbe124d436436bd23a5a5cf1d37afc293c
gangahasan <rgangabhavani2@gmail.com>
2024-12-26 17:03:10 +0530

    questions added to the quiz page

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -2,6 +2,8 @@ import { baseurl } from "./baseurl.js";
 
 let form = document.getElementById("form");
 
+
+
 form.addEventListener("submit", function (event) {
   console.log("clicked");
   event.preventDefault();
@@ -20,6 +22,31 @@ form.addEventListener("submit", function (event) {
     correctoption,
     reviewStatus,
   };
+
+  fetch(`${baseurl}/questions`)
+  .then((res)=res.json())
+  .then((data) => {
+    data.map((el, i) => {
+let card = document.createElement("div");
+      let title = document.createElement("h3");
+      let optionA = document.createElement("h2")
+      let optionB = document.createElement("h2")
+        let optionC = document.createElement("h2")
+        let optionD = document.createElement("h2")
+    let correctOption = document.createElement("h2")
+
+    title.textContent=`Title: ${el.title}`;
+    optionA.textContent=`Option A: ${el.optionA}`;
+    optionB.textContent=`Option B: ${el.optionB}`;
+    optionC.textContent=`Option C: ${el.optionC}`;
+    optionD.textContent=`Option D: ${el.optionD}`;
+    correctOption.textContent=`Correct Option: ${el.correctoption}`;
+
+    card.append(title,optionA,optionB,optionC,optionD,correctOption);
+
+    let maincont = document.getElementById("maincont");
+    maincont.append(card);
+    });
 
   fetch(`${baseurl}/questions`)
     .then((res) => res.json())
