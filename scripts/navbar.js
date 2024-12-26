6f6b6e9c6c3039d8221e8e0bb6e3598d02d4bab1
gangahasan <rgangabhavani2@gmail.com>
2024-12-26 15:22:05 +0530

    created all the required files and add baseurl

---
+++ scripts/navbar.js
@@ -0,0 +1,11 @@
+
+const navbar=()=>{
+
+    let card =`<nav>
+    <a href="./index.html">Home</a>
+    <a href="./quiz.html">Quiz</a>
+    <a href="./questions.html">Questions</a>
+    </nav>`;
+
+
+}
