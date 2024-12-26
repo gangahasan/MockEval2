b525de5fbda31f0ac1a08db4433128733aab1517
gangahasan <rgangabhavani2@gmail.com>
2024-12-26 16:40:05 +0530

    created new question

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -1,7 +1,48 @@
+import { baseurl } from "./baseurl.js";
 
-let form = document.getElementById('form');
+let form = document.getElementById("form");
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    let title = form.question.value;
-})
+form.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let title = form.question.value;
+  let optionA = form.optionA.value;
+  let optionB = form.optionB.value;
+  let optionC = form.optionC.value;
+  let optionD = form.optionD.value;
+  let correctoption = form.correct.value;
+  let quesObj = {
+    title,
+    optionA,
+    optionB,
+    optionC,
+    optionD,
+    correctoption,
+    reviewStatus,
+  };
+
+  fetch(`${baseurl}/questions`)
+  .then((res)=>res.json())
+  .then((data)=>{
+    let question = data.map((el,i)=>{
+        return el.title === title;
+    })
+
+        if(question.length >0){
+            alert("Question already exists");
+            window.location.href = "questions.html";
+        }
+        else{
+            fetch(`${baseurl}/questions`, {
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: JSON.stringify(quesObj),
+            })
+           .then(()=>{
+            alert("Question Created")
+           });
+        }
+    })
+  })
+});
